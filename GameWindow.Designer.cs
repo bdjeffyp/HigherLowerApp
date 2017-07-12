f9c647fbf7d83a9f0c4c0821077306d7264dbf19
Jeff Peterson <bdjeffyp@users.noreply.github.com>
2017-07-12 15:57:04 -0700

    Initial commit

---
+++ GameWindow.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace HigherLowerApp
+{
+    partial class GameWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textAnswer = new System.Windows.Forms.TextBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(259, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number Guessing Game";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Garamond", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(183, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pick a number between 0-100:";
+            // 
+            // textAnswer
+            // 
+            this.textAnswer.Font = new System.Drawing.Font("Garamond", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAnswer.Location = new System.Drawing.Point(202, 55);
+            this.textAnswer.Name = "textAnswer";
+            this.textAnswer.Size = new System.Drawing.Size(70, 24);
+            this.textAnswer.TabIndex = 0;
+            this.textAnswer.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 90);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(260, 23);
+            this.labelStatus.TabIndex = 2;
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelStatus.TextChanged += new System.EventHandler(this.labelStatus_TextChanged);
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewGame.Location = new System.Drawing.Point(13, 117);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(105, 33);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(167, 117);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(105, 33);
+            this.btnGuess.TabIndex = 1;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // GameWindow
+            // 
+            this.AcceptButton = this.btnGuess;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 170);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.textAnswer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "GameWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guessing Game";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameWindow_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textAnswer;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnGuess;
+    }
+}
+
---
+++ GameWindow.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+
+namespace HigherLowerApp
+{
+    public partial class GameWindow : Form
+    {
+        // Private data memebers
+        private int guessCounter;
+        private int numberToGuess;
+        private Random randomizer;
+        private System.Timers.Timer timer;
+        private bool afterTimer;
+        private bool winner;
+
+        // Constants
+        private const int MIN_NUMBER = 0;
+        private const int MAX_NUMBER = 100;
+        private const double DELAY = 3000.0;
+        
+        // Properties
+        public int GuessCounter
+        {
+            get
+            {
+                return guessCounter;
+            }
+            set
+            {
+                guessCounter = value;
+            }
+        }
+
+        public int NumberToGuess
+        {
+            get
+            {
+                return numberToGuess;
+            }
+            set
+            {
+                numberToGuess = value;
+            }
+        }
+
+        public GameWindow()
+        {
+            // Make the window appear
+            InitializeComponent();
+
+            // Initialize the timer
+            timer = new System.Timers.Timer(DELAY);
+            timer.Elapsed += TimerElapsed;
+            timer.AutoReset = false;    // Should only fire once
+            timer.Stop();               // Only enable when needed
+
+            // Turn on the afterTimer flag because we haven't used it yet
+            afterTimer = true;
+
+            // Initialize the guessCounter to 0.
+            guessCounter = 0;
+
+            // Initialize the random number generator and pick a new number.
+            randomizer = new Random();
+            numberToGuess = randomizer.Next(MIN_NUMBER, MAX_NUMBER);
+
+            // The player hasn't won yet!
+            winner = false;
+
+            // Ensure the status label is clear
+            labelStatus.Text = "";
+
+            // Place focus on the text box
+            textAnswer.Focus();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            // We are starting a new game.
+
+            // Reinitialize the guessCounter
+            guessCounter = 0;
+
+            // Set up the timer
+            timer.Stop();
+            afterTimer = true;
+
+            // Clear the text in the status label
+            labelStatus.Text = "";
+
+            // Reseed the random number generator
+            randomizer = new Random();
+            numberToGuess = randomizer.Next(MIN_NUMBER, MAX_NUMBER);
+
+            // Reset the winner flag
+            winner = false;
+
+            // Restore the guess button
+            btnGuess.Enabled = true;
+
+            // Put focus on the text box
+            textAnswer.Focus();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            // The user made a guess!
+
+            // Increment the guess counter, even if it was a bad input.
+            guessCounter++;
+
+            // Highlight the text in the text box to allow for immediate guess entry
+            textAnswer.SelectAll();
+
+            // Store the answer in a temp integer.
+            int answer = 0;
+            if (int.TryParse(textAnswer.Text, out answer) == false)
+            {
+                labelStatus.Text = "Not a number!";
+                return;
+            }
+
+            // Verify the guess is within the correct range.
+            if (answer < MIN_NUMBER || answer > MAX_NUMBER)
+            {
+                labelStatus.Text = "Guess out of range!";
+                return;
+            }
+
+            // Check the input versus the number to guess.
+            if (answer < numberToGuess)
+            {
+                labelStatus.ForeColor = Color.Red;
+                labelStatus.Text = "Higher!";
+            }
+            else if (answer > numberToGuess)
+            {
+                labelStatus.ForeColor = Color.Red;
+                labelStatus.Text = "Lower!";
+            }
+            else if (answer == numberToGuess)
+            {
+                labelStatus.ForeColor = Color.Green;
+                labelStatus.Text = "Correct! Score: " + guessCounter;
+                // Disable guess button to force a new game.
+                btnGuess.Enabled = false;
+                // Set the winner flag so that the label stays up
+                winner = true;
+            }
+            else
+            {
+                labelStatus.Text = "BUG!!!";
+            }
+        }
+
+        private void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // After 3 seconds, clear the status label's text.
+            labelStatus.Text = "";
+
+            // Allow the timer to be used again if the label changes
+            afterTimer = true;
+        }
+
+        private void labelStatus_TextChanged(object sender, EventArgs e)
+        {
+            // Only use the timer to clear if not a winner
+            if (winner == false)
+            {
+                // Start the timer so that it clears the text 3 seconds later
+                if (afterTimer == true)
+                {
+                    // This means that the timer has been used and needs to be used again
+                    timer.Start();
+                    afterTimer = false;
+                }
+                else
+                {
+                    // This means that the timer is still in use so reset the timer
+                    timer.Stop();
+                    timer.Start();
+                }
+            }            
+        }
+
+        private void GameWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Release the timer
+            timer.Dispose();
+        }
+    }
+}
